053f8debfc8509464d8bd408f16e9d0a2e4d8a86
Dhananjay Balan <mail@dbalan.in>
2020-08-15 09:08:04 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {}
+, ghc ? "ghc865"
+}:
+
+pkgs.stdenv.mkDerivation rec {
+  name = "recipes";
+
+#  nativeBuildInputs = [
+#    pkgs.pkg-config
+#  ];
+
+  buildInputs = [
+    pkgs.zlib
+    pkgs.haskell.compiler.${ghc}
+    pkgs.cabal-install
+    pkgs.haskellPackages.ghcid
+  ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH
+    export LANG=en_US.UTF-8
+  '';
+}
