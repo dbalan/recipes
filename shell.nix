66841df9ed343e281d3a318cc1d48ac8f04a0ab8
Dhananjay Balan <mail@dbalan.in>
2021-08-02 22:26:09 +0200

    Add .gitignore

--- shell.nix
+++ shell.nix
@@ -1,5 +1,4 @@
-{ pkgs ? import <nixpkgs> {}
-, ghc ? "ghc884"
+{ pkgs ? import <unstable> {}
 }:
 
 pkgs.stdenv.mkDerivation rec {
@@ -11,7 +10,8 @@ pkgs.stdenv.mkDerivation rec {
 
   buildInputs = [
     pkgs.zlib
-    pkgs.haskell.compiler.${ghc}
+    pkgs.haskell-language-server
+    # pkgs.haskell.compiler.${ghc}
     pkgs.cabal-install
     pkgs.haskellPackages.ghcid
     pkgs.haskellPackages.cabal-plan
